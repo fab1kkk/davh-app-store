e4f15cafaba715b18530cc1e053791e9da581a96
Fabik <szczawik1997@gmail.com>
2023-03-19 13:19:32 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name')}}</title>
+</head>
+
+<body>
+    @include('navbar')
+    <main>
+        <article>
+            <p><strong>{{ config('app.name') }}</strong> project using PHP v<strong>{{ PHP_VERSION }}</strong> and Laravel v<strong>{{ Illuminate\Foundation\Application::VERSION}}</strong></p>
+        </article>
+        {{ $project_name }}
+    </main>
+</body>
+
+</html>
